221a96e450fc56b095ffc579de3a1c9191e7abe1
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-07-29 10:54:19 +0530

    creating favourites provider

---
+++ lib/providers/favourites_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
+
+  void toggleMealFavouriteStatus(Meal meal) {
+    final mealIsFavourite = state.contains(meal);
+
+    if (mealIsFavourite) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final favouriteMealssProvider =
+    StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>(
+  (ref) => FavouriteMealsNotifier(),
+);
